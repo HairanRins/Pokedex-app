7ba6626b8f5ed7712260e2f9f338bb463d2e7063
rins <rinsart127@gmail.com>
2025-03-08 16:29:55 +0300

    advance 2.1.1

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,39 @@
 import { Card } from "@/components/Card";
 import { PokemonCard } from "@/components/pokemon/PokemonCard";
+import { Row } from "@/components/Row";
+import { SearchBar } from "@/components/SearchBar";
 import { ThemedText } from "@/components/ThemedText";
 import { getPokemonId } from "@/functions/pokemon";
-import { useFetchQuery } from "@/hooks/useFetchQuery";
+import { useFetchQuery, useInfinitefetchQuery } from "@/hooks/useFetchQuery";
 import { useThemeColors } from "@/hooks/useThemeColors";
-import { FlatList, Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { ActivityIndicator, FlatList, Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   const colors = useThemeColors();
-  const {data} = useFetchQuery('/pokemon?limit=21');
-  const pokemons = data?.results ?? []
+  const {data, isFetching, fetchNextPage} = useInfinitefetchQuery('/pokemon?limit=21');
+  const pokemons = data?.pages.flatMap(page => page.results) ?? []
+  const [search, setSearch] = useState('')
   return (
     <>
       <SafeAreaView style={[styles.container, {backgroundColor: colors.tint}]}>
-        <View style={styles.header}>
+        <Row style={styles.header} gap={16}>
           <Image source={require("@/assets/images/pokeball.png")}width={24} height={24} />  
           <ThemedText variant="headline" color="light">Pokédex</ThemedText>
-        </View>
+        </Row>
+        <Row>
+        <SearchBar value={search} onChange={setSearch} />
+        </Row>
         <Card style={styles.body}>
           <FlatList 
             data={pokemons}
             numColumns={3}
             contentContainerStyle={[styles.gridGap, styles.list]}
             columnWrapperStyle={styles.gridGap}
+            ListFooterComponent={
+              isFetching ? <ActivityIndicator color={colors.tint}/> : null
+            }
+            onEndReached={() => fetchNextPage}
             renderItem={({ item }) => ( 
                 <PokemonCard id={getPokemonId(item.url)} name={item.name} style={{ flex: 1/3}}/>
             )} 
@@ -40,24 +51,25 @@ const styles = StyleSheet.create (
     container : {
       flex: 1,
       padding:4,
+      gap:16,
       // justifyContent: "center",
       // alignItems: "center",
     },
     header: {
-      flexDirection : 'row',
-      alignItems: 'center',
-      gap: 16,
-      padding: 12,
+      paddingHorizontal: 12,
+      paddingVertical: 8,
     },
     body: {
       flex: 1,
+      marginTop:16,
     },
     gridGap: {
       gap:8,
     },
     list: {
       padding: 12,
-    }
+    },
+    
   }
 )
 
--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,7 +1,8 @@
-import { Image ,StyleSheet,View,ViewStyle } from "react-native"
+import { Image ,Pressable,StyleSheet,View,ViewStyle } from "react-native"
 import { Card } from "@/components/Card";
 import { ThemedText } from "../ThemedText";
 import { useThemeColors } from "@/hooks/useThemeColors";
+import { Link } from "expo-router";
 
 type Props = {
     style?: ViewStyle,
@@ -11,15 +12,19 @@ type Props = {
 
 export function PokemonCard({style,id,name}: Props) {
     const colors = useThemeColors();
-    return <Card style={style}>
-        <ThemedText style={styles.id} variant="caption" color="medium">#{id.toString().padStart(3, '0')}</ThemedText>
-        <Image 
-            source={{uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}}
-            style={{ width: 72, height: 72 }}
-        />
-        <ThemedText>{name}</ThemedText>
-        <View style={[styles.shadow, {backgroundColor: colors.gray.background}]}/>
-    </Card>
+    return  <Link href={{pathname: "/pokemon/[id]", params: {id: id}}} asChild>
+        <Pressable android_ripple={{color:colors.tint, foreground:true}} style={style}>
+            <Card style={[styles.card]}>
+                <ThemedText style={styles.id} variant="caption" color="medium">#{id.toString().padStart(3, '0')}</ThemedText>
+                <Image 
+                    source={{uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}}
+                    style={{ width: 72, height: 72 }}
+                />
+                <ThemedText>{name}</ThemedText>
+                <View style={[styles.shadow, {backgroundColor: colors.gray.background}]}/>
+            </Card>
+        </Pressable>
+    </Link>
 }
 
 const styles = StyleSheet.create({
