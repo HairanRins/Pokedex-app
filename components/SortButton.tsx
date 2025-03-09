a6cb21a70f180613993ef00847bd805ac0435f97
rins <rinsart127@gmail.com>
2025-03-09 11:09:23 +0300

    advance 2.1.3

--- components/SortButton.tsx
+++ components/SortButton.tsx
@@ -1,25 +1,44 @@
 import { useThemeColors } from "@/hooks/useThemeColors"
-import { useState } from "react"
-import { Image, Modal, Pressable, StyleSheet, Text, View } from "react-native"
+import { useRef, useState } from "react"
+import { Dimensions, Image, Modal, Pressable, StyleSheet, Text, View } from "react-native"
+import { ThemedText } from "./ThemedText"
+import { Card } from "@/components/Card";
+import { Row } from "@/components/Row";
+import { Radio } from "@/components/Radio";
+import { Shadows } from "@/constants/Shadows";
+
 
 type Props = {
     value: "id" | "name",
     onChange: (v: "id" | "name") => void
 }
 
+const options = [
+    {label: "Number", value: "id"},
+    {label: "Name", value: "name"},
+] as const;
+
 export function SortButton({value, onChange}: Props) {
-    const colors = useThemeColors()
+    const buttonRef = useRef<View>(null);
+    const colors = useThemeColors();
     const [isModalVisible, setModalVisibility] = useState(false)
+    const [position, setPosition] = useState<null | {top: number, right: number}>(null);
     const onButtonPress = () => {
-        setModalVisibility(true)
-    }
+        buttonRef.current?.measureInWindow((x, y, width, height) => {
+            setPosition({
+                top: y + height,
+                right: Dimensions.get("window").width - x - width
+            })
+            setModalVisibility(true)
+        })
+    };
     const onClose = () => {
         setModalVisibility(false)
-    }
+    };
     return (
         <>
             <Pressable onPress={onButtonPress}>
-                <View style={[styles.button, {backgroundColor: colors.gray.white}]}>
+                <View ref={buttonRef} style={[styles.button, {backgroundColor: colors.gray.white}]}>
                     <Image 
                         source={value == "id" ? require("@/assets/images/tag.png") : require("@/assets/images/alpha.png")}
                         width={16}
@@ -29,6 +48,15 @@ export function SortButton({value, onChange}: Props) {
             </Pressable>
             <Modal transparent visible={isModalVisible} onRequestClose={onClose}>
                 <Pressable style={styles.backdrop} onPress={onClose} />
+                <View style={[styles.popup, {backgroundColor: colors.tint, ...position}]}>
+                    <ThemedText style={styles.title} variant="subtitle2" color="white">Sort by:</ThemedText>
+                    <Card style={styles.card}>
+                        {options.map((o) => ( <Pressable onPress={() => onChange(o.value)}><Row key={o.value} gap={8}>
+                            <Radio checked={o.value == value}/>
+                            <ThemedText>{o.label}</ThemedText>
+                        </Row></Pressable>))}
+                    </Card>
+                </View>
             </Modal>
         </>
     )
@@ -46,6 +74,22 @@ const styles = StyleSheet.create({
     backdrop: {
         flex: 1,
         backgroundColor: "rgba(0,0,0,0.3)",
-        
+    },
+    popup: {
+        position: 'absolute',
+        width: 113,
+        padding: 4,
+        paddingTop: 16,
+        gap:16,
+        borderRadius: 12,
+        ...Shadows.dp2,
+    },
+    title: {
+        paddingLeft:20
+    },
+    card: {
+        paddingVertical:16,
+        paddingHorizontal:20,
+        gap:16,
     }
 })
